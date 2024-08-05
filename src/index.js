8957a43d07cf3db20add2ff7056d1a96d2f217e4
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2024-08-05 13:47:58 +0300

    feat: complete the task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,26 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = ''
+
+    for(let i = 0; i < expr.length; i+=10) {
+        const chunk = expr.slice(i,i+10)
+
+        if(chunk === '**********') {
+            result += ' '
+        }else {
+            let morseCode = '';
+
+            for(let j = 0; j < chunk.length; j+=2) {
+                const symbol = chunk.slice(j,j+2)
+                if(symbol === '10') morseCode += '.'
+                if(symbol === '11') morseCode += '-'
+            }
+            result += MORSE_TABLE[morseCode]
+        }
+    }
+
+    return result;
 }
 
 module.exports = {
